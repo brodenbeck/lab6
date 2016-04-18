607d5bf186a9c90e65833ba78e7ccc60a2834a4d
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2016-04-18 13:25:43 -0400

    initial commit

---
+++ temperature.js
@@ -0,0 +1,17 @@
+// bonus: get these inputs from the user using a prompt
+
+function convertTemp(temp, toUnit) {
+	if(toUnit === "F") {
+		convertedTemp = (temp * 9/5 + 32);
+		console.log(temp + " degrees Celsius converts to " + convertedTemp + " degrees Fahrenheit.");
+	}
+	else if(toUnit === "C") {
+		convertedTemp = (temp - 32) * 5 / 9;
+		console.log(temp + " degrees Fahrenheit converts to " + convertedTemp + " degrees Celsius.");
+	}
+	else {
+		console.log('Please enter your temperature unit as either Celsius ("C") or Fahrenheit ("F").')
+	}
+}
+
+convertTemp(32, "C");
