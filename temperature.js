2aa980403d8d93ac40a8ea788e0c5664d412bd6c
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2016-04-22 16:56:39 -0400

    added js/html communication and styled using stylus

--- temperature.js
+++ temperature.js
@@ -1,18 +1,13 @@
-function convertTemp(temp, toUnit) {
-	if(toUnit === "F") {
-		convertedTemp = (temp * 9/5 + 32);
-		console.log(temp + " degrees Celsius converts to " + convertedTemp + " degrees Fahrenheit.");
-	}
-	else if(toUnit === "C") {
-		convertedTemp = (temp - 32) * 5 / 9;
-		console.log(temp + " degrees Fahrenheit converts to " + convertedTemp + " degrees Celsius.");
+var button = document.getElementById("button");
+
+button.addEventListener("click", function () {
+	var unit = document.getElementById("units").value;
+	var temperature = Number(document.getElementById("temperature").value, 10);
+	var displayedTemp = document.getElementById("conversion");
+	if(unit === "fahrenheit") {
+		displayedTemp.innerText = (temperature + " degrees Celsius converts to " + (temperature * 9/5 + 32) + " degrees Fahrenheit.");
 	}
 	else {
-		console.log('Please enter your temperature unit as either Celsius ("C") or Fahrenheit ("F"). Refresh the page to restart.')
+		displayedTemp.innerText = (temperature + " degrees Fahrenheit converts to " + (temperature - 32) * 5 / 9 + " degrees Celsius.");
 	}
-}
-
-var temp = prompt("What temperature do you want to convert from? Enter just the number.");
-var toUnit = prompt('What unit of temperature do you want to convert to? Enter either "F" or "C".');
-
-convertTemp(temp, toUnit);
+});
